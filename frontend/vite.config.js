618486ec16a29821bdb1d2fa26f440ee9a4e34e9
Sakshee <sakshee.zala@simformsolutions.com>
2026-06-17 17:10:08 +0530

    chore: prepare application for CI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5174,
     host: true
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
   }
-});
+})
